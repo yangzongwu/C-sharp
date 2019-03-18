f8ac7153f54a5b6d9703dfd50bfdd4e22621eac0
zongwu <yangzong5@gmail.com>
2019-03-18 09:27:56 -0700

    Update List.cs

--- Basic/List.cs
+++ Basic/List.cs
@@ -1,6 +1,18 @@
 //初始化
-List<T> mList = new List<T>();
+List<T>()	
+//Initializes a new instance of the List<T> class that is empty and has the default initial capacity.
+//List<T> mList = new List<T>();
 List<int> mList = new List<int>() { 1, 2, 3 };
+
+List<T>(IEnumerable<T>)	
+//Initializes a new instance of the List<T> class that contains elements copied from the specified 
+//collection and has sufficient capacity to accommodate the number of elements copied.
+string[] input = { "Brachiosaurus", "Amargasaurus", "Mamenchisaurus" };
+List<string> dinosaurs = new List<string>(input);
+
+List<T>(Int32)	
+Initializes a new instance of the List<T> class that is empty and has the specified initial capacity.
+
 
 //add
 List.Add(T item);
