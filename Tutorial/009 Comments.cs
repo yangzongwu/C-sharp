602d8319a2ebd0707b796677c1eb574524e2cb0e
zongwu <yangzong5@gmail.com>
2019-03-19 13:01:46 -0700

    Create 009 Comments.cs

---
+++ Tutorial/009 Comments.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace part1_ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //single line
+            /* multi line
+              line 2
+              line 3*/
+             //ctrl+E,ctrl+C
+             //ctrl+K,ctrl+U
+            /// xml documentation comments
+
+
+        }
+    }
+
+    /// <summary>
+    /// This is a sample class and a sample documents
+    /// </summary>
+    public class class2
+    {
+
+    }
+
+}
