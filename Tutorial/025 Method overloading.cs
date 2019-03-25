f9c989c827e49cad7fa14da1b0b8ecdede8285e6
zongwu <yangzong5@gmail.com>
2019-03-25 00:28:04 -0700

    Create 025 Method overloading.cs

---
+++ Tutorial/025 Method overloading.cs
@@ -0,0 +1,48 @@
+//C# functions can be overloaded based on the number,type(int,float etc) 
+//and kind(Value,Ref,Out) of parameters
+
+
+using System;
+class Program
+{
+    public static void Main()
+    {
+        Add(1,2);
+    }
+    public static void Add(int FN, int SN)
+    {
+        Console.WriteLine("Sum={0}", FN + SN);
+    }
+    public static void Add(int FN, int SN,int TN)
+    {
+        Console.WriteLine("Sum={0}", FN + SN);
+    }
+    public static void Add(float FN, float SN)
+    {
+        Console.WriteLine("Sum={0}", FN + SN);
+    }
+    public static void Add(int FN, float SN)
+    {
+        Console.WriteLine("Sum={0}", FN + SN);
+    }
+}
+
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Add(1,2,3);
+    }
+    public static void Add(int FN, int SN,int TN)
+    {
+        Console.WriteLine("Sum={0}", FN + SN);
+    }
+    public static void Add(int FN, int SN, out int Sum)
+    {
+        Console.WriteLine("Sum={0}", FN + SN);
+        Sum = FN + SN;
+    }
+}
