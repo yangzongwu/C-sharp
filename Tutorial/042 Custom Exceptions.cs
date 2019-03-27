947541cfb7e33f1a655d9587ad87b9e6a0c16e73
zongwu <yangzong5@gmail.com>
2019-03-26 20:02:25 -0700

    Update 042 Custom Exceptions.cs

--- Tutorial/042 Custom Exceptions.cs
+++ Tutorial/042 Custom Exceptions.cs
@@ -1,4 +1,23 @@
 /*
+Custom Exception steps
+1.Create a class that derives from System.Exception class.As a convention,end the 
+class name with Exception suffic,All .NET exceptions end with, exception suffix.
+
+2.Provide a public constructor,taht takes in a string parameter.This constructor
+simply passes the string parameter,to the base exception class constructor.
+
+3.Using innerExceptions,you can also track back the original exception,If you want to
+provide this capability for your custom exception class, then overload the constructor accordingly
+
+r.If you want your Exception class object to work across application domains,then the 
+object must be serializable.To make you exception class serializable mark it with Serializable
+attribute and provide a constructor that invokes the base Exception class constructor that
+takes in SerializetionInfo and StreamingContext object as parameters.
+
+*/
+
+
+/*
 I have an asp.net web application
 the application should allow the user to have only one logged in session
 if hte user is already loggin , and if he opens another brower window and tries to 
