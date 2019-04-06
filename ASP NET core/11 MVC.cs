4c6650f183d9c66c357e93b55daec5d710afca0c
zongwu <yangzong5@gmail.com>
2019-04-06 15:36:02 -0700

    Update 11 MVC.cs

--- ASP NET core/11 MVC.cs
+++ ASP NET core/11 MVC.cs
@@ -58,7 +58,93 @@ public class EmployeeController:EmployeeController
     }
     
  //===============Summary=================================================================
+ /*
  MVC is an architectural design pattern for implementing User Interface Layer of an applicaiton
  Model : Set of classes that represent data + the logic to manage that data
  View  : Contains the display logic to present the Model data provided to it by the Controller
  Controller : Handles the http request,work with the model, and selects a view to render that model
+ */
+ 
+ 
+
+
+
+
+//===============set up MVC=================================================================
+//Step 1: Add the MVC Services to the Dependency Injection Container
+public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();  //?AddMvc vs AddMvcCore
+        }
+//step 2: Add MVC middleware to the Request Pipeline
+public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            { app.UseDeveloperExceptionPage(); }
+  
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+ 
+
+//===============set up MVC=================================================================
+//Add folder under project named Controllers
+//under Controllers add HomeController.cs
+//------------HomeController.cs-----------------
+namespace EmployeeManagement.Controllers{
+    public class HomeController{
+        public string Index()
+        { return "Hello from MVC"; }
+    }
+}
+//--------------Startup.cs-----------------
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        { _config = config; }
+
+        public void ConfigureServices(IServiceCollection services)
+        { services.AddMvc(); }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            { app.UseDeveloperExceptionPage();  }
+
+            app.UseStaticFiles();
+          
+          // will process HomeController.cs and return ,will not process the Run() Func
+          // so the web will shows: Hello from MVC
+            app.UseMvcWithDefaultRoute(); 
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Helloword");
+            //});
+        }
+    }
+}
+
+
+
+//================AddMvc vs AddMvcCore======================================================================
+public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(); 
+        }
+
+public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore();  
+        }
+/*
+AddMvcCore() method only adds the core MVC services
+AddMvc() method adds all the required MVC services
+AddMvc() method calls AddMvcCore() method internally
+
+*/
