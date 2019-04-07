472ed124f2c227bad876d0c5630486e4fdc39fb0
zongwu <yangzong5@gmail.com>
2019-04-07 01:11:29 -0700

    Add files via upload

---
+++ ASP NET core/15 EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepsitory)
+        {
+            _employeeRepository = employeeRepsitory;
+        }
+
+        public ViewResult Index()
+        {
+            var model= _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+        public ViewResult Details()
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Employee Details"
+            };
+            return View(homeDetailsViewModel);
+        }
+    }
+}
---
+++ ASP NET core/15 EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    //If not use interface, we can not use dependency injection as a result our application
+    //will be extremely difficult to change and maintain unit testing also become very difficult
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1,Name="Mary",Department="HR",Email="dfds@gmail.com"},
+                new Employee(){Id=2,Name="John",Department="IT",Email="SD@gmail.com"},
+                new Employee(){Id=3,Name="Mike",Department="IT",Email="SDFS@gmail.com"}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
